ada259b40b6ac7e9234ac17482f4da84a2b44d1c
bloodoregame <pro.bloodore@gmail.com>
2025-05-14 13:46:35 +0200

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -1,8 +1,15 @@
 import express, { type Request, Response, NextFunction } from "express";
 import { registerRoutes } from "./routes";
 import { setupVite, serveStatic, log } from "./vite";
+import cors from 'cors';
 
 const app = express();
+app.use(cors({
+  origin: process.env.NODE_ENV === 'production' 
+    ? process.env.ALLOWED_ORIGINS?.split(',') 
+    : true,
+  credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
